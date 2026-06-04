f0922d7608c03b7e050c63438cff877d9ad3b709
forzik1337 <gameforzi2000@gmail.com>
2026-06-04 15:09:43 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -681,4 +681,4 @@ threading.Thread(target=run_bot, daemon=True).start()
 threading.Thread(target=auto_loop, daemon=True).start()
 threading.Thread(target=stream_watcher, daemon=True).start()
 
-app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
